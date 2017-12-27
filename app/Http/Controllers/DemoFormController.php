c95cb891480397811942af0d7f456d6f197f4549
cheyuwu <frank890417@gmail.com>
2017-12-27 14:53:24 +0800

    update json key error

--- app/Http/Controllers/DemoFormController.php
+++ app/Http/Controllers/DemoFormController.php
@@ -14,14 +14,14 @@ class DemoFormController extends Controller
     public function getFormData(){
         $inputs =  Input::all();
         $inputs["response"]= (array_key_exists('recommand',$inputs) && $inputs['recommand']=="yes")?"感謝你的熱愛":"嗚嗚很難過你不喜翻";
-        if ($inputs['json']==true){
+        if (array_key_exists('json',$inputs) && $inputs['json']){
             return $inputs;
         }
         $res = "<h3>後端收到了資料：</h3><ul>";
         $res = $res."<li>名字:".$inputs['name']."</li>";
         $res = $res."<li>商品:".(array_key_exists('option',$inputs)?$inputs['option']:"未選擇")."</li>";
         $res = $res."<li>心得:".$inputs['comment']."</li>";
-        $res = $res."<li>推薦:".($inputs['recommand']?"是":"否")."</li>";
+        $res = $res."<li>推薦:".($inputs['recommand']=="yes"?"是":"否")."</li>";
         $res = $res."<li>系統回覆:".$inputs['response']."</li>";
         $res = $res."<li>原始資料:".json_encode($inputs)."</li>";
 
